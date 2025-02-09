911339fc8e21480b6000689703997c35a4e3c113
Guillermo Moral <willy.moral@gmail.com>
2025-02-09 19:47:10 -0300

    Add a user

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,7 +1,11 @@
 package com.gm
 
 import com.mongodb.kotlin.client.coroutine.MongoClient
+import data.model.User
+import data.model.dataSource.MongoUserDataSource
 import io.ktor.server.application.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
@@ -16,6 +20,26 @@ fun Application.module() {
     val mongoClient = MongoClient.create(connectionString)
     val database = mongoClient.getDatabase(databaseName)
 
+    val userDataSource = MongoUserDataSource(database)
+
+    GlobalScope.launch {
+
+        val dummyMediaList = ArrayList<String>()
+        dummyMediaList.add("1")
+        dummyMediaList.add("2")
+        dummyMediaList.add("3")
+
+        val user = User(
+            name = "test name",
+            email = "test@gmail.com",
+            hash = "test hash",
+            salt = "test salt",
+            mediaList = dummyMediaList
+        )
+
+        userDataSource.insertUser(user)
+
+    }
 
     configureMonitoring()
     configureSerialization()
